bf764155b8f9870e6c806a52e5705462e9161df5
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-08-23 20:10:41 +0300

    add: countdown

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 
 ## Svolgimento
 
-1. ora corrente
-2. l'ora di domani in millisecondi
-3. setInterval a 1secondo
-4. sottrarre ora corrente - ora domani
-5. calcolare giorni, ora, minuti e secondi in base alla sottrazione
-6. riportare tutto nel dom
-7. fermare il cronometro quando arriva a 0
+1. setInterval a 1secondo
+    - ora corrente
+    - l'ora di domani in millisecondi
+    - sottrarre ora domani - ora corrente
+    - calcolare giorni, ora, minuti e secondi in base alla sottrazione
+    - riportare tutto nel dom
+    - fermare il cronometro quando arriva a 0
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
 
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+
+const countdown = setInterval(function() {
+
+    // Ora corrente
+    const now = new Date();
+
+    // L'ora in cui finirà il countdown
+    const end = new Date('Aug 24, 2023 10:30:00').getTime();
+
+   //Differenza di tempo
+    let distance = end - now;
+    distance--;
+
+    // Trovare giorni, ora, minuti e secondi leggibili 
+    const readableDays = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const readableHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const readableMinutes =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const readableSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //Riportare tutto nel DOM
+    days.innerHTML = readableDays;
+    hours.innerHTML = readableHours;
+    minutes.innerHTML = readableMinutes;
+    seconds.innerHTML = readableSeconds;
+
+    //stoppare quando finice il countdown
+    if(distance < 0) {
+        clearInterval(countdown);
+    }
+    
+}, 1000);
+
+
